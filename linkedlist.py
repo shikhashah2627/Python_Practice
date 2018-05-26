ad99ee45e9ef1449c082c78e2ffa1b3e72e73954
Shikha Shah <shikha2@fab01.cecs.pdx.edu>
2018-05-25 20:42:26 -0700

    linked list_adding_element_at initial point

--- linkedlist.py
+++ linkedlist.py
@@ -1,17 +1,37 @@
+#creation of linked list
+
 class Node:
-    def __init__(self,initdata):
-        self.data = initdata
-        self.next = None
+    def __init__(self, dataval=None):
+        self.dataval = dataval
+        self.nextval = None
 
-    def getData(self):
-        return self.data
+class SLinkedList:
+    def __init__(self):
+        self.headval = None
 
-    def getNext(self):
-        return self.next
+    def list_print(self):
+        printval = self.headval
+        while printval is not None:
+            print (printval.dataval)
+            printval = printval.nextval    
 
-    def setData(self,newdata):
-        self.data = newdata
+    def add_at_begining(self,data):
+        new_data = Node(data)
+        new_data.nextval = self.headval
+        self.headval = new_data
 
-    def setNext(self,newnext):
-        self.next = newnext
+list = SLinkedList()
+list.headval = Node("Mon")
+e2 = Node("Tue")
+e3 = Node("Wed")
 
+list.headval.nextval = e2
+e2.nextval = e3
+
+list.add_at_begining("Sun")
+
+list.list_print()
+
+
+#using import i.e. exisitng linked list data structure from python
+#         
