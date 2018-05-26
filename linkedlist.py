2a7b7f990922661ee30637628915d0a94298d80f
Shikha Shah <shikha2@fab01.cecs.pdx.edu>
2018-05-25 21:25:32 -0700

    Added the code to add item in the end

--- linkedlist.py
+++ linkedlist.py
@@ -20,6 +20,18 @@ class SLinkedList:
         new_data.nextval = self.headval
         self.headval = new_data
 
+    def add_at_end(self,data):
+        new_data = Node(data)
+        if self.headval is None:
+            self.headval = new_data
+            return
+        last = self.headval
+        while(last.nextval):
+            last = last.nextval
+        last.nextval=new_data
+        
+    
+
 list = SLinkedList()
 list.headval = Node("Mon")
 e2 = Node("Tue")
@@ -29,9 +41,11 @@ list.headval.nextval = e2
 e2.nextval = e3
 
 list.add_at_begining("Sun")
+list.add_at_end("Thur")
 
 list.list_print()
 
 
 #using import i.e. exisitng linked list data structure from python
-#         
+# pip install llist 
+#from llist import dllist, dllistnode    
