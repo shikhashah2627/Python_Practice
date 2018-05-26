d3e8337dc911ad3cb2982c884d4c119ca5d106b4
Shikha Shah <shikha2@fab01.cecs.pdx.edu>
2018-05-25 21:34:07 -0700

    added the code to

--- linkedlist.py
+++ linkedlist.py
@@ -5,7 +5,7 @@ class Node:
         self.dataval = dataval
         self.nextval = None
 
-class SLinkedList:
+class Single_LinkedList:
     def __init__(self):
         self.headval = None
 
@@ -20,6 +20,7 @@ class SLinkedList:
         new_data.nextval = self.headval
         self.headval = new_data
 
+    #to add element in the end
     def add_at_end(self,data):
         new_data = Node(data)
         if self.headval is None:
@@ -29,18 +30,25 @@ class SLinkedList:
         while(last.nextval):
             last = last.nextval
         last.nextval=new_data
-        
+
+    def add_in_between(self,data,mid_data):
+        new_data = Node(data)
+        if mid_data is None:
+            return
+        new_data.nextval = mid_data.nextval
+        mid_data.nextval = new_data
     
 
-list = SLinkedList()
-list.headval = Node("Mon")
-e2 = Node("Tue")
-e3 = Node("Wed")
+
+list = Single_LinkedList()
+list.headval = Node("1")
+e2 = Node("2")
+e3 = Node("5")
 
 list.headval.nextval = e2
 e2.nextval = e3
 
-list.add_at_begining("Sun")
+list.add_at_begining("6")
 list.add_at_end("Thur")
 
 list.list_print()
